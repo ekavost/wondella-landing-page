96ef6a7ef239fa360d41566c70f56ec5ddb08979
Ekaterina Vostrikova <ekavos@hotmail.com>
2025-03-28 13:28:04 +0100

    The header layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-app-bar app>
+  <v-app-bar app shrink-on-scroll prominent :src="imgUrl">
+    <v-container fluid class="d-flex justify-space-between align-center">
+      <v-img contain max-width="120" :src="logoUrl"></v-img>
+      <v-toolbar-title class="white--text text-uppercase">
+        {{ title }}
+      </v-toolbar-title>
+      <v-btn color="#9E9E9E">¡Reserve ahora!</v-btn>
+      <!-- TODO:
+      Añadir opción de compartir en Facebook and x
+      https://developers.facebook.com/docs/sharing/reference/share-dialog -->
+    </v-container>
     <template v-slot:extension>
       <the-nav-bar></the-nav-bar>
     </template>
@@ -10,9 +20,22 @@
 import TheNavBar from "@/components/TheNavBar.vue";
 
 export default {
-  name: "the-header",
+  name: "TheHeader",
   components: {
     TheNavBar,
   },
+  data() {
+    return {
+      title: "Titulo de viaje",
+      imgUrl: "https://picsum.photos/1920/1080?random",
+      logoUrl: require("@/assets/logo/wondella-logo.png"),
+    };
+  },
 };
 </script>
+
+<style scoped>
+.v-btn {
+  color: white;
+}
+</style>
